f29104316917e63f9112e69849fe0dd4aa524ddc
mar10j <youthefluff@gmail.com>
2014-08-21 18:09:16 -0400

    Started engine
    not even close to being done

---
+++ trpg engine.py
@@ -0,0 +1,7 @@
+up = up.lower().strip()
+down = down.lower().strip()
+right = right.lower().strip()
+left = left.lower.strip()
+if up == ("up"):
+    print("You Decide to head upward")
+
---
+++ trpg project.py
@@ -0,0 +1,8 @@
+up = input ("What way do you want to go?")
+
+if up == ("up"):
+    print("You Decide to head upward")
+if up == ("right"):
+    print("You Decide to head right")
+
+
