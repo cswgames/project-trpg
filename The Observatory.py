eaa0a2f1d689f4c601917156b91cbac7a69d30c2
mar10j <youthefluff@gmail.com>
2014-08-23 23:29:07 -0400

    STOP BREAKING THE 4TH WALL, JEEZ.

---
+++ The Observatory.py
@@ -0,0 +1,11 @@
+obser = input ("Welcome to the observatory. Here you can look out over the vast world. would you like to do it? Y/N").strip().lower()
+if obser == ("y"):
+    input("Alright take a look!")
+    input("You see a vast, colorful land, full of nature. You stand there, upset that this is a text based RPG, and you can't actually see it. Haha. -cswgames")
+    input("Looks like it's time to leave!")
+if obser == ("n"):
+    input("Oh well, Goodbye then!")
+if obser != ("y"):
+    input("YES OR NO QUESTION. Jeez, some people.")
+if obser != ("n"):
+    input("YES OR NO QUESTION. Jeez, some people.")
