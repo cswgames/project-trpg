57a3b0a883a0f283ed1786885661220e910b0da8
wimuhlbach <applesarefum@gmail.com>
2014-08-24 18:04:47 -0400

    YAAAAAAAAAAAAAAAAAAAAY
    Can't wait to finally learn how to use the command that does hat I want
    it to

--- The pines.py
+++ The pines.py
@@ -5,6 +5,7 @@ input ("silence")
 input ("you light the candle aside your bed")
 input ("and prepare for your day")
 input ("You entered The Pines")
+#1
 pine = input("There's the Blacksmith, the Pond and the Forest. Where would you like to go?")
 
 
@@ -19,37 +20,19 @@ if pine == ('blacksmith'):
     if pinehelpo == ("i3qr"):
         input("He is saved because of you! here take this LVL 1 Armor, It will help on your quest.")
         input("You decide to go to the forest.")
-        di = input("Ton Castle is infront of you, Deeper Woods are to your right, The Pines are behind you, and The Mountains are to your left")
-        if di == ("up"):
-            input("You Decide to head upward")
-        if di == ("right"):
-            input("You Decide to head right")
-        if di == ("left"):
-            input("You Decide to head left")
-        if di == ("down"):
-            input("You Decide to head downward")
-
+       #Go to #1
+    if pinehelpo != ("i3qr")
+        input ("No! that's no help!")
+        #Go to #1
 
 if pine == ('pond'):
     input("You decide to head to the pond")
     pinehelp = input("Please help us! My son Edward was attacked by a snake! Go to the medic in Ton Castle and get the code from him!")
     if pinehelp == ("i3qr"):
         input("He is saved because of you! here take this LVL 1 Armor, It will help on your quest.")
-        piner = input("Where would you like to go?")
-        if piner == ('blacksmith'):
-            input ("You decide to head to the Blacksmith.")
-            input ("You Got a LVL 1 Sword!")
-            input("You decide to go to the forest.")
-            di = input("Ton Castle is infront of you, Deeper Woods are to your right, The Pines are behind you, and The Mountains are to your left")
-            if di == ("up"):
-                input("You Decide to head upward")
-            if di == ("right"):
-                input("You Decide to head right")
-            if di == ("left"):
-                input("You Decide to head left")
-            if di == ("down"):
-                input("You Decide to head downward")
-
+    else:
+        input ("That's no help!")
+        pinehelp
 
 if pine == ('forest'):
     input("You decide to go to the forest.")
