f0c1c347a721eb1c22cf2cccbfe079800d4d7df4
mar10j <youthefluff@gmail.com>
2014-08-23 20:00:54 -0400

    UNFINISHED, DON'T BE CONFUSED ABOUT PROBLEMS
    I know all the problems and I will get to them all soon.

--- The pines.py
+++ The pines.py
@@ -4,12 +4,29 @@ pine = input("Here we have a Blacksmith, Pond and Forest. Where would you like t
 if pine == ('blacksmith'):
     input ("You decide to head to the Blacksmith.")
     input ("You Got a LVL 1 Sword!")
+    pineo = input ("Where would you like to go?")
+    if pineo == ("pond"):
+        input("You decide to head to the pond")
+    pinehelpo = input("Please help us! My son Edward was attacked by a snake! Go to the medic in Ton Castle and get the code from him!")
+    if pinehelpo == ("i3qr"):
+        input("He is saved because of you! here take this LVL 1 Armor, It will help on your quest.")
+        input("Where would you like to go?")
+    
+
+
+
 
 if pine == ('pond'):
     input("You decide to head to the pond")
     pinehelp = input("Please help us! My son Edward was attacked by a snake! Go to the medic in Ton Castle and get the code from him!")
     if pinehelp == ("i3qr"):
         input("He is saved because of you! here take this LVL 1 Armor, It will help on your quest.")
+        input("Where would you like to go?")
+        
+
+
+
+
 
 if pine == ('forest'):
     input("You decide to go to the forest.")
