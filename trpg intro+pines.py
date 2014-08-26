55b35430952061d188dc72bb61d05ce517915968
wimuhlbach <applesarefum@gmail.com>
2014-08-25 21:07:17 -0400

    Update
    ~Fixed some grammatical errors
    ?Need help with lists

--- trpg intro+pines.py
+++ trpg intro+pines.py
@@ -16,7 +16,13 @@ input ('''
 name = input('''                               What's your name?
 ''').strip().title()
 yn = input('Oh! So your name is ' +name +' right? (Y/N)').strip().title()
-yna = ('Y','N')
+#
+yna = ('YN')
+
+if yn == yna:
+    input ("Sorry, I don't know what that means, answer Y for yes or N for no")
+    #Back to "So your name is___________?"
+
 if yn == ('N'):
     input ("Oh?")
     input ("So that's not your name?")
@@ -25,10 +31,6 @@ if yn == ('N'):
     input ("Or instead, here's an idea, you could tell me your REAL name!")
     input ("Sound good?")
     #Back to "What's your name?"
-
-if yn != ('N','Y'):
-    input ("Sorry, I don't know what that means, answer Y for yes or N for no")
-    #Back to "So your name is___________?"
 
 if yn == ('Y'):
     print ('''GAME START''')
@@ -51,43 +53,43 @@ if yn == ('Y'):
 
     if pine == ('blacksmith'):
         input ("You decide to head to the Blacksmith.")
-    if swlvl < 1:
-        swlvl = 1
-        input ("You Got a LVL 1 Sword!")
-        pineo = input ("Where would you like to go?")
-    if pineo == ('pond'):
-        input("You decide to head to the pond")
-        pinehelpo = input("Please help us! My son Edward was attacked by a snake! Please, go to the medic in Tau Castle and get the code from him!")
-    if pinehelpo == ("i3qr"):
-        input("He is saved because of you! here take this LVL 1 Armor, It will help on your quest.")
-        input("You decide to go to the forest.")
-        #Go to #1
-    if pinehelpo != ("i3qr"):
-        input ("No! that's no help!")
-        #Go to #1
+        if swlvl < 1:
+            swlvl = 1
+            input ("You Got a LVL 1 Sword!")
+            pineo = input ("Where would you like to go?")
+        if pineo == ('pond'):
+            input("You decide to head to the pond")
+            pinehelpo = input("Please help us! My son Edward was attacked by a snake! Please, go to the medic in Tau Castle and get the code from him!")
+        if pinehelpo == ("i3qr"):
+            input("He is saved because of you! here take this LVL 1 Armor, It will help on your quest.")
+            input("You decide to go to the forest.")
+            #Go to #1
+        if pinehelpo != ("i3qr"):
+            input ("No! that's no help!")
+            #Go to #1
     
-    if pine == ('pond'):
-    input("You decide to head to the pond")
-    pinehelp = input("Please help us! My son Edward was attacked by a snake! Go to the Witch Doctor in Swamp and get the code from him!")
-    #2
-    if pinehelp == ("i3qr"):
-        input("He is saved because of you! here take this LVL 1 Armor, It will help on your quest.")
-    else:
-        input ("That's no help!")
-        pinehelp = input ("What is the code?!")
-        #Go to #2
+        if pine == ('pond'):
+            input("You decide to head to the pond")
+            pinehelp = input("Please help us! My son Edward was attacked by a snake! Go to the Witch Doctor in Swamp and get the code from him!")
+            #2
+            if pinehelp == ("i3qr"):
+                input("He is saved because of you! here take this LVL 1 Armor, It will help on your quest.")
+            else:
+                input ("That's no help!")
+                pinehelp = input ("What is the code?!")
+                #Go to #2
 
-    if pine == ('forest'):
-    input("You decide to go to the forest.")
-    di = input("Tau Castle is infront of you, the deep woods are to your right, The Pines are behind you, and the mountains are to your left")
-    if di == ("up"):
-        input("You Decide to head forward, to Tau Castle")
-    if di == ("right"):
-        input("You Decide to head right, into the Deep Woods")
-    if di == ("left"):
-        input("You Decide to head left, into the mountain ranges")
-    if di == ("down"):
-        input("You Decide to head downward")
+        if pine == ('forest'):
+            input("You decide to go to the forest.")
+            di = input("Tau Castle is infront of you, the deep woods are to your right, The Pines are behind you, and the mountains are to your left")
+            if di == ("up"):
+                input("You venture towards Tau Castle")
+            if di == ("right"):
+                input("You head right, into the deep woods")
+            if di == ("left"):
+                input("You Decide to head left, into the mountain range")
+            if di == ("down"):
+                input("You Decide to head back to The Pines")
 
 
     
