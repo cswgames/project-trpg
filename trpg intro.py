5dfe8fe9e65cc03a816d2e815b29b4c8484f95e1
William Benjamin Muhlbach <applesarefum@gmail.com>
2014-08-27 17:21:02 -0400

    hello

--- trpg intro.py
+++ trpg intro.py
@@ -21,13 +21,13 @@ if yn == ('Y'):
 #Game goes here
     
 if yn == ('N'):
-    input ("Oh?")
-    input ("So that's not your name?")
-    input ("Well then...")
-    input ("I suppose we could sit here for a while and do nothing")
-    input ("Or instead, here's an idea, you could tell me your REAL name!")
-    input ("Sound good?")
+    input("Oh?")
+    input("So that's not your name?")
+    input("Well then...")
+    input("I suppose we could sit here for a while and do nothing")
+    input("Or instead, here's an idea, you could tell me your REAL name!")
+    input("Sound good?")
 #Back to "What's your name?"
 if yn != ('N','Y'):
-    input ("Sorry, I don't know what that means, answer Y for yes or N for no")
+    input("Sorry, I don't know what that means, answer Y for yes or N for no")
 #Back to "So your name is___________?"
