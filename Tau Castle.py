322966fef80b217df348969c0a0c0a7e77d145ed
William Benjamin Muhlbach <applesarefum@gmail.com>
2014-09-03 17:15:03 -0400

    stuff happened

--- Tau Castle.py
+++ Tau Castle.py
@@ -1,28 +1,39 @@
 #Whole thing needs != statements, Thats for you william. :D
+def noentry()
+    print("You've already been here.")
+    print("Where would you like to go?")
 name = input ("name needs a variable so put your name in, this will only be here until we put all these parts in the final product.").strip().title()
 input("Welcome to Tau Castle.")
 tau = input("Here we have a market, Royal hall, blacksmith, prison and the road. Where would you like to go?").strip().lower()
-if tau == ("blacksmith"):
+if tau == "blacksmith":
     input("You decide to go to the Blacksmith")
-    taub = input("Mega deal! What would you like a better sword (LVL 2) or a bow(LVL 1)?").strip().lower()
-    if taub == ("sword"):
-        input("One sword for you " + name)
-    if taub == ("bow"):
-        input("One bow for you " + name)
-
-
-if tau == ("market"):
+    if bstau = 0
+        noentry
+    elif
+        taub = input("Mega deal! What would you like a better sword (LVL 2) or a bow(LVL 1)?").strip().lower()
+            if taub == "sword":
+                input("One sword for you " + name)
+                swordlvl = 2
+            if taub == "bow":
+                input("One bow for you " + name)
+                bowlvl = 1
+    bstau = 1
+    
+if tau == "market":
     input("You decide to go to the market.")
+
     taucha = input("Just as you are looking at the food you hear a loud scream, and a man running. Theif! Do you want to chase him or back off?").strip().lower()
-    if taucha == ("chase"):
-        input("You decide to chase after the theif")
-        #Combat section once we get that, prize LVL 2 armor
-    if taucha == ("back off"):
-        input("You decide to back off")
-        #No prize
-
-
-if tau == ("prison"):
+        if taucha == "chase":
+            input("You decide to chase after the theif")
+            #Combat section once we get that, prize LVL 2 armor
+        if taucha == "back off":
+            input("You decide to back off")
+            #No prize
+        mtau = 1
+    elif 
+    noentry
+    
+if tau == "prison":
     input("You decide to go to the Prison.")
     taupris = input ("Hey, I know where a lot of treasure is. You help me escape, and we will split it. Y/N").strip().lower()
     if taupris == ("y"):
