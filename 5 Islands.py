feb74b8c96c217ce8b1e73a77806deb3312ec596
wimuhlbach <applesarefum@gmail.com>
2014-09-13 09:42:40 -0400

    well, it's a start
    the game will forcibly make you go through the islands in the order of
    A, B, C, D, E.

---
+++ 5 Islands.py
@@ -0,0 +1,38 @@
+def 5islands():
+    5isyn = input("Are you sure that you'd like to begin this side quest?(Y/N)").strip().title()
+    if 5isyn == ("N"):
+        #this depends on where we put the entrance.
+    if 5isyn == ("Y"):
+       5is()
+    elif:
+        print ("I'm sorry, I'm not sure what that means, answer Y for yes or N for no.")
+        5islands()
+#----------------------------------------------------------------------------------
+def 5is():
+    input("You set out to sea, the calmness of the water is relaxing and soothing.")
+    input("In all directions water is the only thing visible.")
+    5isyn = input("You see land, sail towards land? (Y/N)")
+    if 5isyn == ("Y"):
+        input("You start to sail towards the island.")
+        5isarrive
+    if 5isyn == ("N"):
+        input("You begin to sail back to the land you came from.")
+        5isarrive()
+    elif:
+        print ("I'm sorry, I'm not sure what that means, answer Y for yes or N for no.")
+        5is()
+
+
+
+
+        def 5isarrive()
+        input("The wind picks up")
+        input("Rain falls over the water.")
+        input("The crashing waves are too much for the little boat.")
+        input("You pass out...")
+        input("...darkness...")
+        input("...blackness...")
+        input("You awaken, dazed and confused.")
+        input("You see the remains of what you assume is a sailboat, lying on the ground, damaged beyond repair.")
+        input("
+
