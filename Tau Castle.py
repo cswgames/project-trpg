5f0aa7cd50b219ab9cdcd31532546ea3e4ba00f0
wimuhlbach <applesarefum@gmail.com>
2014-09-14 10:17:39 -0400

    wheeeeeeeeeeeeeeeeeee
    i don't remember making edits

--- Tau Castle.py
+++ Tau Castle.py
@@ -1,52 +1,52 @@
 #Whole thing needs != statements, Thats for you william. :D
-def noentry()
-    print("You've already been here.")
-    print("Where would you like to go?")
+
 name = input ("name needs a variable so put your name in, this will only be here until we put all these parts in the final product.").strip().title()
 input("Welcome to Tau Castle.")
 tau = input("Here we have a market, Royal hall, blacksmith, prison and the road. Where would you like to go?").strip().lower()
 if tau == "blacksmith":
     input("You decide to go to the Blacksmith")
-    if bstau = 0
-        noentry
-    elif
+    if bstau = 1:
+        noentry()
+    else:
         taub = input("Mega deal! What would you like a better sword (LVL 2) or a bow(LVL 1)?").strip().lower()
             if taub == "sword":
                 input("One sword for you " + name)
-                swordlvl = 2
-            if taub == "bow":
+                swlvl = 2
+            elif taub == "bow":
                 input("One bow for you " + name)
-                bowlvl = 1
+                bolvl = 1
     bstau = 1
     
-if tau == "market":
+elif tau == "market":
     input("You decide to go to the market.")
 
-    taucha = input("Just as you are looking at the food you hear a loud scream, and a man running. Theif! Do you want to chase him or back off?").strip().lower()
+    taucha = input("As you look at the rows and rows of food, you hear a loud scream and a man running. Thief! Do you want to chase him or back off?").strip().lower()
         if taucha == "chase":
             input("You decide to chase after the theif")
             #Combat section once we get that, prize LVL 2 armor
-        if taucha == "back off":
+        elif taucha == "back off":
             input("You decide to back off")
             #No prize
         mtau = 1
     elif 
     noentry
     
-if tau == "prison":
+elif tau == "prison":
     input("You decide to go to the Prison.")
     taupris = input ("Hey, I know where a lot of treasure is. You help me escape, and we will split it. Y/N").strip().lower()
     if taupris == ("y"):
         input("Thank you so much! First thing you need to do is get the code from the royal hall, then come back and enter the code and we will be on our way!")
         ("If you know the code type it in here, if you don't know it type in bye")
+    elif
+    else
 
 
-if tau == ("road"):
+elif tau == ("road"):
     input("You Decide to leave Tau Castle and get back on the road.")
 #Put in Navi Engine and where ever the road we connect this place to.
 
 
-if tau == ("royal hall"):
+elif tau == ("royal hall"):
     input("You Decide to head to the Royal Hall.")
     input('You look around, You see a painting with the letters "fhr4s" on it anda gaurd who has the letters "jwq7y"')
     input("You Decide to leave")
