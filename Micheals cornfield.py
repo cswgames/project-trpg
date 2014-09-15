f4c77885b409c6a05327376a34338eabd1758734
soren0077 <goobinator007@gmail.com>
2014-09-15 16:58:57 -0400

    corn field started
    I started the corn field

---
+++ Micheals cornfield.py
@@ -0,0 +1,11 @@
+lr=input("left or right?").strip().lower()
+
+if lr == ("left"):
+    print ("You deside to head to the left")
+
+    elif lr == ("right"):
+        print ("You deside to head to the right")
+
+    else:
+        print("I am sorry, I do not understand.")
+
