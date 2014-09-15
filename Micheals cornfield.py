34585d9eb3471ce0a810f9d38524edfc36977fa6
soren0077 <goobinator007@gmail.com>
2014-09-15 18:18:04 -0400

    micheals insulting corn field deal with it

--- Micheals cornfield.py
+++ Micheals cornfield.py
@@ -1,11 +1,51 @@
-lr=input("left or right?").strip().lower()
+def cornfieldpoopinasock():
+    wrongvar = input ("You deside to enter the corn maze, would you like to continue? or go back to the road.")
+    if wrongvar == ("continue"):
+        input ("You decide to continue into the maze. ")
 
-if lr == ("left"):
-    print ("You deside to head to the left")
-
-    elif lr == ("right"):
-        print ("You deside to head to the right")
+    elif wrongvar == ("go back"):
+        input ("You decide to head back. ")
 
     else:
-        print("I am sorry, I do not understand.")
+        print ("I am sorry I do not understand, please use english. ")
+    cornfieldpoopinasock()
+cornfieldpoopinasock()
+
+
+
+
+def leftorright1stplaceloser():
+    leftorright1stplaceloser = input("you continue into the maze would you like to go left right or back to the start").strip().lower()
+
+    if  leftorright1stplaceloser == ("left"):
+        input ("You deside to go left")
+
+    elif leftorright1stplaceloser == ("right"):
+        input ("You deside to go right")
+
+    else:
+        print ("I am sorry I do not understand why you can not type")
+    
+    leftorright1stplaceloser()
+leftorright1stplaceloser()
+
+
+
+def firstplaceleftforwardorrighthampickle():
+    fungijim = input ("You deside to continue into the maze, would you like to go forwards, the path to the right, or back to the last decition")
+
+    if firstplaceleftforwardorrighthampickle == ("forward"):
+        input ("You continue heading forward")
+
+    elif firstplaceleftforwardorrighthampickle == ("right"):
+        input ("You deside to head down the right path.")
+
+    elif firstplaceleftforwardorrighthampickle == ("go back"):
+        input ("You head back to your last decition")
+
+    else:
+        print ("I am sorry you are to stupid to understand")
+    firstplaceleftforwardorrighthampickle()
+firstplaceleftforwardorrighthampickle()
+
 
