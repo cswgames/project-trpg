a8734b65259cdaa34fb5ef2df168e9f95a34ac51
mar10j <youthefluff@gmail.com>
2014-09-15 16:46:03 -0400

    Start of the farm, do not edit yet Will
    not even close to being done

---
+++ Micheals farm.py
@@ -0,0 +1,9 @@
+#Jack made this, William you need to do the add points thing like you did in the pines  here.
+def farmhub():
+        frub = input("Welcome to Micheals farm, Here we have a, Farm, Cornfield or back on the road, Where do you want to go? > ").strip().lower()
+        if frub == "farm":
+            input("You decide to head to the farm.")
+            mikeiscool = input('"Hello there! Did you come to help me work?" [Type yes or no]')
+            if mikeiscool == "No":
+                input("Oh ok, Goodbye then!")
+                
