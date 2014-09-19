553e5578ecbcef2e469709d9202d9b38794b108a
soren0077 <goobinator007@gmail.com>
2014-09-19 17:40:34 -0400

    i MADE i POOP OOPS CAPS i DON'T CARE

--- Micheals cornfield.py
+++ Micheals cornfield.py
@@ -20,10 +20,10 @@ def leftorright1stplaceloser():
 
     if  leftorright1stplaceloser == ("left"):
         input ("You deside to go left")
-
+        firstplaceleftforwardorrighthampickle()
     elif leftorright1stplaceloser == ("right"):
         input ("You deside to go right")
-
+        rightpathbananacreampineapple()
     else:
         print ("I am sorry I do not understand why you can not type")
     
@@ -35,13 +35,13 @@ leftorright1stplaceloser()
 def firstplaceleftforwardorrighthampickle():
     fungijim = input ("You deside to continue into the maze, would you like to go forwards, the path to the right, or back to the last decition")
 
-    if firstplaceleftforwardorrighthampickle == ("forward"):
+    if fungijim == ("forward"):
         input ("You continue heading forward")
-
-    elif firstplaceleftforwardorrighthampickle == ("right"):
+        winorloseorbacksideleftgalafray()
+    elif fungijim == ("right"):
         input ("You deside to head down the right path.")
 
-    elif firstplaceleftforwardorrighthampickle == ("go back"):
+    elif fungijim == ("go back"):
         input ("You head back to your last decition")
 
     else:
@@ -50,6 +50,20 @@ def firstplaceleftforwardorrighthampickle():
 firstplaceleftforwardorrighthampickle()
 
 
+
+def deadendrubberwall():
+    flubberblubber = input ("oh no ran into a dead end. would you like to go back to last intersection, or back to the start?")
+
+    if flubberblubber == ("go back"):
+        input ("You  deside to head back to the last section")
+
+    elif flubberblubber == ("beginning"):
+        input ("you head back the way you came")
+
+    else:
+        print ("this here is a failior to co mu no cate")
+    deadendrubberwall()
+deadendrubberwall()
 
 
 
