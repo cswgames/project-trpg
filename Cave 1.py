1b4dc5952cab4b71f4f203d728cf5b5db5d89286
mar10j <youthefluff@gmail.com>
2014-09-19 17:55:24 -0400

    YUMMY.

--- Cave 1.py
+++ Cave 1.py
@@ -3,12 +3,13 @@ def caveswagg():
     cavety = input("It's Dark. You decide that there is no going back. Do you want to keep going or leave while you have the chance? Type 'leave' or 'keep going' >").strip().lower()
     if cavety == "leave":
         input("You leave the cave.")
+        #load the road funct. 
     elif cavety == "keep going":
         input("You decide to keep going.")
+        cavesavee()
     else:
         input("I do not understand")
     caveswagg()
-caveswagg()
 
 def cavesavee():
     cavety2 = input("You travel deep into the cave, you hit a wall and there is a split hall, Do you go right or left? >")
@@ -16,13 +17,15 @@ def cavesavee():
         input("You head over to the left")
         input("You start to see light, you continue. You find a Treasure Chest. Inside you found a LVL 3 Bow!")
         input("You now leave the cave.")
-         #Give a LVL 3 Bow to player
+         #Give a LVL 3 Bow to player and load the road funct
     elif cavety2 == "right":
         input("You head over to the right.")
         input("You see light, you continue foward. As you are walking you feel a sharp pain in your back. The blade is pulled out from your spine. You fall down.")
         input("GAME OVER")
+        #load the main menu
     else:
         input("I do not understand")
     cavesavee()
-cavesavee()
 
+
+caveswagg()
