9591e70d01910c354ba1ad0253d12634f6987a25
soren0077 <goobinator007@gmail.com>
2014-09-19 19:31:53 -0400

    my first cave

---
+++ cave 3.444.py
@@ -0,0 +1,39 @@
+def caveoftheroundrubbleroom():
+    input ("you enter a dark cave...")
+
+    sniperpigeoncarrotbomb = input ("as you enter the cave you see a torch on the wall would you like to take it?").strip().lower()
+    if sniperpigeoncarrotbomb == ("take"):
+        input ("you decide to take the torch")
+        takentothechuhauhauden()
+    elif sniperpigeoncarrotbomb == ("leave it"):
+        input ("you leave the torch and continue.")
+        dontyoutakemyllama()
+    else:
+        input ("I am sorry I do not understand.")
+        caveoftheroundrubbleroom()
+
+
+
+def takentothechuhauhauden():
+    input ("you continue down the path")
+
+    fullgoatbladder = input ("you continue through the dark cave with your torch...")
+    input ("along the way you find LVL. 5 bow...")
+    input ("after a few more turns you find a exit.")
+
+
+def dontyoutakemyllama():
+    input("You continue down the path")
+
+    fullyackbladder = input ("you keep walking through the cave and see a light...")
+    input ("you run to the light...")
+    input ("but you trip on a turtle and hit your head on a stone...")
+    input ("GAMEOVER")
+
+
+
+
+
+
+
+caveoftheroundrubbleroom()
