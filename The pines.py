8e3cf6d2cfb259ad8a3cc4ef877715339871c7dd
wimuhlbach <applesarefum@gmail.com>
2014-09-19 17:46:08 -0400

    sooooo much got dune!
    a lot got done as well!!

--- The pines.py
+++ The pines.py
@@ -5,51 +5,53 @@ input ("silence")
 input ("you light the candle aside your bed")
 input ("and prepare for your day")
 input ("You enter The Pines")
-#1
-pine = input("There's the Blacksmith, the Pond and the Forest. Where would you like to go?")
-
-pinep = ('blacksmith','pond','forest')
-
-if pine is not pinep:
-    input ("lol")
-    #Go to #1
-
-if pine == ('blacksmith'):
-    input ("You decide to head to the Blacksmith.")
-    if swlvl < 1:
-        swlvl = 1
-    input ("You Got a LVL 1 Sword!")
-    pineo = input ("Where would you like to go?")
-    if pineo == ('pond'):
-        input("You decide to head to the pond")
-    pinehelpo = input("Please help us! My son Edward was attacked by a snake! Please, go to the medic in Tau Castle and get the code from him!")
-    if pinehelpo == ("i3qr"):
-        input("He is saved because of you! here take this LVL 1 Armor, It will help on your quest.")
-        input("You decide to go to the forest.")
-       #Go to #1
-    if pinehelpo != ("i3qr"):
-        input ("No! that's no help!")
-        #Go to #1
-
-if pine == ('pond'):
-    input("You decide to head to the pond")
-    pinehelp = input("Please help us! My son Edward was attacked by a snake! Go to the Witch Doctor in Swamp and get the code from him!")
-    #2
+def pinehelpfunc():
     if pinehelp == ("i3qr"):
         input("He is saved because of you! here take this LVL 1 Armor, It will help on your quest.")
     else:
         input ("That's no help!")
         pinehelp = input ("What is the code?!")
-        #Go to #2
-
-if pine == ('forest'):
-    input("You decide to go to the forest.")
+        pinehelpfunc():
+            
+def pineforest
     di = input("Tau Castle is infront of you, the deep woods are to your right, The Pines are behind you, and the mountains are to your left")
     if di == ("up"):
-        input("You Decide to head forward, to Tau Castle")
-    if di == ("right"):
-        input("You Decide to head right, into the Deep Woods")
-    if di == ("left"):
+        input("You Decide to head onward to Tau Castle")
+    elif di == ("right"):
+        input("You Decide to head right, into the deep woods")
+    elif di == ("left"):
         input("You Decide to head left, into the mountain ranges")
-    if di == ("down"):
-        input("You Decide to head downward")
+    elif di == ("down"):
+        input("You Decide to head back to the Pines")
+        pines():
+
+def pines():
+    pine = input("There's the Blacksmith, the Pond and the Forest. Where would you like to go?")
+    if pine == ('blacksmith'):
+        input ("You decide to head to the Blacksmith.")
+        if swlvl < 1:
+            swlvl = 1
+        input ("You Got a LVL 1 Sword!")
+        pineo = input ("Where would you like to go?")
+        if pineo == ('pond'):
+            input("You decide to head to the pond")
+            pinehelpo = input("Please help us! My son Edward was attacked by a snake! Please, go to the medic in Tau Castle and get the code from him!")
+            if pinehelpo == ("i3qr"):
+                input("He is saved because of you! here take this LVL 1 Armor, It will help on your quest.")
+                input("You decide to go to the forest.")
+                pines():
+            else:
+                input ("No! that's no help!")
+                pines():
+
+    elif pine == ('pond'):
+        input("You decide to head to the pond")
+        pinehelp = input("Please help us! My son Edward was attacked by a snake! Go to the Witch Doctor in Swamp and get the code from him!")
+        pinehelpfunc()
+    elif pine == ('forest'):
+        input("You decide to go to the forest.")
+
+    else:
+        input ("Sorry, I'm not sure what that means")
+        pines():
+pines():
