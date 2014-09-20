9771bbc3926dc24ed85ad0da5974201c1b505748
soren0077 <goobinator007@gmail.com>
2014-09-20 16:03:55 -0400

    RUNE 1 WILLIAM ADD MONSTERS OOPS CAPS

--- rune 1.py
+++ rune 1.py
@@ -1,14 +1,14 @@
 def runeofthechubaca():
-    input ("you enter a mysterius rune. ")
+    input ("you are in mysterius temple. ")
 
-    bobthecan = input ("you see 2 paths would you like to go left or right. ")
+    bobthecan = input ("there are 2 paths, would you like to go left or right. ")
 
     if bobthecan == ("left"):
         input ("you decide to head to the left. ")
         rune1leftfunnydog()
     elif bobthecan == ("right"):
         input ("you deside to head to the right. ")
-
+        jackhasnoswag()
     else:
         input ("I am sorry I do not understand. ")
     runeofthechubaca()
@@ -16,24 +16,55 @@ def runeofthechubaca():
 #decition left when you enter ----------------------------------------------
 
 def rune1leftfunnydog():
-    input ("you find a key to the crypt and you continue walking")
+    input ("you find a key {1509} to the crypt and you continue walking")
     input ("you find a coradoor to the right side...")
-    input ("you find a crate with a lock, look around for a key")
-
-
+    input ("you find a crate with a lock, look around for a key. ")
 
 
     blanketofsnow = input ("you need to find the key to the box would you like to continue or go back, to the door")
     
     if blanketofsnow == ("continue"):
         input ("you head forward in search for the key. ")
-
-    if blanketofsnow == ("go back"):
+        theswarmofbeavers()
+#enemy for the area is called "the swarm"
+    elif blanketofsnow == ("go back"):
         input ("you go back to see if you can get into the crypt. ")
+        rune1leftfunnydog()
 
-#decition 1 going to the right------------------------------------------------
+#decition to go to the right------------------------------------------------
 
-def rightwaywrongdoor
+def jackhasnoswag():
+    input ("you reach the crypt door")
+
+    jackneedstoshower = input ("if you know the key code type it in, if not go back. ")
+
+    if jackneedstoshower == ("1509"):
+        input ("you enter the crypt")
+        cryptofthetaco()
+
+    elif jackneedstoshower == ("go back"):
+        input ("you head back to the start to find the key. ")
+        runeofthechubaca()
+
+    else:
+        input ("I am sorry I do not understand")
+        jackhasnoswag()
+
+#crypt function--------------------------------------------------------------
+
+def cryptofthetaco():
+    input ("you enter the crypt to find the swarm overlord. ")
+#if win ("you find chest key 12667")
+#if lose GAMEOVER
+
+
+#after battle function for blanketofsnow-------------------------------------------------------
+
+
+def theswarmofbeavers():
+    input ("oh no you encounter a swarmberserker! ")
+#if win ("you find a LVL. 8")
+#if lose GAMEOVER
 
 
 
