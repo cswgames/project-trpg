55f5195261441422c005dcd32cc9de7d5b7a6f9a
wimuhlbach <applesarefum@gmail.com>
2014-09-20 11:42:38 -0400

    Also...
    This wasn't as bad as some of your code.

--- 5 Islands/The Observatory.py
+++ 5 Islands/The Observatory.py
@@ -1,5 +1,5 @@
-def pikachuisagangster():
-    obser = input ("Welcome to the observatory. Here you can... well... obsreve. Would you like to? Y/N").strip().lower()
+def observatory():
+    obser = input ("Welcome to the observatory. Here you can... well... observe. Would you like to? Y/N").strip().lower()
     if obser == ("y"):
         input("Alright take a look!")
         print()
@@ -42,18 +42,14 @@ def pikachuisagangster():
         input("Overwhelmed by the vast beauty")
         input("Amazed that in this world")
         input("Full of insanity and chaos")
-        input("That you were able to find beauty")
+        input("That you were able to find such beauty")
         input("The landscape stretches back as far as your eye can see")
-        input("You're upset that this is a text based RPG, and you can't actually see anything. Haha. -cswgames")
+        input("You're upset that this is a text based RPG, and you can't actually see anything")
         input("Looks like it's time to leave!")
     elif obser == ("n"):
         input("Oh well, Goodbye then!")
         #load the road wherever this is XD
     else:
         print("YES OR NO QUESTION. Jeez, some people.")
-        pikachuisagangster()        
-
-
-
-
-pikachuisagangster()
+        observatory()        
+observatory()
