ac046a575185cd71a9e96adc0bada92792ecb28b
soren0077 <goobinator007@gmail.com>
2014-09-19 22:03:42 -0400

    cave4

---
+++ cave 4 soren.py
@@ -0,0 +1,42 @@
+
+
+def cave4tincansulfer():
+    input ("you wonder into a dark cave. ")
+    input("you find 3 paths")
+
+    beaver = input ("would you like to go down the left, right, or staight path. ").strip().lower()
+
+    if beaver == ("left"):
+            input ("you decide to go left. ")
+            cave4lefttunapickle()
+    elif beaver == ("right"):
+        input ("you decide to go right. ")   
+        cave4rightisnotalwaysright()
+        
+    elif beaver == ("straight"):
+        input ("you head straight forward... ")
+        cave4straightflingtree()
+    else:
+        input ("I am sorry I do not understand. ")
+    cave4tincansulfer()
+
+
+
+
+def cave4lefttunapickle():
+    input ("you find a LVL. 6 sword... ")
+    input ("the celing starts to cave in...")
+    input ("you run for the exit and nearly escape. ")
+
+
+def cave4straightflingtree():
+    input 
+    #redue path split off
+
+
+def cave4rightisnotalwaysright():
+    input ("as you walk through the path the ground opens up from under you. ")
+    input ("you fall into a shadowy area and encounter a")
+    #add monster to line above
+
+cave4tincansulfer()    
