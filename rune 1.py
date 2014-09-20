cd6c02e2cfd32cad27301e9b3feeb72c6533d3ff
soren0077 <goobinator007@gmail.com>
2014-09-20 15:11:57 -0400

    rune 1  started

---
+++ rune 1.py
@@ -0,0 +1,41 @@
+def runeofthechubaca():
+    input ("you enter a mysterius rune. ")
+
+    bobthecan = input ("you see 2 paths would you like to go left or right. ")
+
+    if bobthecan == ("left"):
+        input ("you decide to head to the left. ")
+        rune1leftfunnydog()
+    elif bobthecan == ("right"):
+        input ("you deside to head to the right. ")
+
+    else:
+        input ("I am sorry I do not understand. ")
+    runeofthechubaca()
+
+#decition left when you enter ----------------------------------------------
+
+def rune1leftfunnydog():
+    input ("you find a key to the crypt and you continue walking")
+    input ("you find a coradoor to the right side...")
+    input ("you find a crate with a lock, look around for a key")
+
+
+
+
+    blanketofsnow = input ("you need to find the key to the box would you like to continue or go back, to the door")
+    
+    if blanketofsnow == ("continue"):
+        input ("you head forward in search for the key. ")
+
+    if blanketofsnow == ("go back"):
+        input ("you go back to see if you can get into the crypt. ")
+
+#decition 1 going to the right------------------------------------------------
+
+def rightwaywrongdoor
+
+
+
+
+runeofthechubaca()
