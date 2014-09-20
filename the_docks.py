bb2d2bf05e42f3b31e74eec01c9f0b76135700e5
mar10j <youthefluff@gmail.com>
2014-09-20 15:42:20 -0400

    DOX DOX DOX

---
+++ the_docks.py
@@ -0,0 +1,29 @@
+def disdockdock():
+    dipdorpdop = input("Welcome to the Docks, Would you like to set sail? or go back to the road? [Type 'sail' or 'road']").strip().lower()
+    if dipdorpdop == "sail":
+        wheredoyouwannasaillahore()
+
+    elif dipdorpdop == "road":
+        print("You leave the docks.")
+        #load road funct.
+
+    else:
+        print("I am sorry, I do not understand.")
+        disdockdock()        
+
+def wheredoyouwannasaillahore():
+    williamhastomuchswag = input('"Hello there mate! Would you like to go to Torch island? or The Five Islands?" [Type "torch" or "five".').strip().lower()
+
+    if williamhastomuchswag == "torch":
+        input('"Alright! I wish you safe travels." The boat sails off.')
+
+    elif williamhastomuchswag == "five":
+        input('"Alright! I wish you safe travels." The boat sails off.')
+
+    else:
+        print("What did you say?")
+        wheredoyouwannasaillahore()
+
+
+
+disdockdock()
