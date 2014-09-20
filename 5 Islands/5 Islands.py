d31234e6f8cc9cf885424057d5fee71c795a2d59
mar10j <youthefluff@gmail.com>
2014-09-20 09:58:50 -0400

    William, Don't do crack and code at the same time.
    XD jk but this was a mess

--- 5 Islands/5 Islands.py
+++ 5 Islands/5 Islands.py
@@ -1,28 +1,29 @@
-def 5islands():
-    5isyn = input("Are you sure that you'd like to begin this side quest?(Y/N)").strip().title()
-    if 5isyn == ("N"):
+def fiveislands():
+    fiveisyn = input("Are you sure that you'd like to begin this side quest?(Y/N)").strip().title()
+    if fiveisyn == ("N"):
+        input("You turn back.")
         #this depends on where we put the entrance.
-    if 5isyn == ("Y"):
-       5is()
-    elif:
+    elif fiveisyn == ("Y"):
+        fiveis()
+    else:
         print ("I'm sorry, I'm not sure what that means, answer Y for yes or N for no.")
-        5islands()
+        fiveislands()
 #----------------------------------------------------------------------------------
-def 5is():
-    input("You set out to sea, the calmness of the water is relaxing and soothing.")
+def fiveis():
+    input("You set out to sea, the calmness of the water is relaxing and soothing.").strip().lower()
     input("In all directions water is the only thing visible.")
-    5isyn = input("You see land, sail towards land? (Y/N)")
-    if 5isyn == ("Y"):
+    fiveisyn = input("You see land, sail towards land? (Y/N)")
+    if fiveisyn == "y":
         print("You start to sail towards the island.")
-        5isarrive
-    if 5isyn == ("N"):
+        fiveisarrive()
+    elif fiveisyn == "n":
         print("You begin to sail back to the land you came from.")
-        5isarrive()
-    elif:
+        fiveisarrive()
+    else:
         print ("I'm sorry, I'm not sure what that means, answer Y for yes or N for no.")
-        5is()
+        fiveis()
 #----------------------------------------------------------------------------------
-def 5isarrive()
+def fiveisarrive():
     print("The wind picks up")
     print("Rain pounds against the water.")
     print("The crashing waves are too much for the little boat.")
@@ -32,3 +33,6 @@ def 5isarrive()
     print("You awaken, dazed and confused.")
     print("You see the remains of what you assume is a sailboat, lying on the ground next to you, damaged beyond repair.")
     
+
+
+fiveislands()
