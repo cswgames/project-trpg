0fe25d47916ae37c9a50df0c37b2fcf7a0dddcbc
soren0077 <goobinator007@gmail.com>
2014-09-24 19:40:01 -0400

    temple 2 started why cruel syntex error just why

---
+++ temple 2.py
@@ -0,0 +1,41 @@
+input ("you enter a dark temple. >")
+
+def templetwotaco():
+    input ("you find a cross round of 3 paths. ")
+   headlesshorsemen = input ("would you like to go left, forward, or right. ")
+
+if headlesshorsemen == ("left"):
+    input ("you deside to go left ")
+
+elif headlesshorsemen == ("forward"):
+    input ("you deside to continue forward ")
+
+elif headlesshorsemen == ("right"):
+    input ("you follow the path to the right ")
+    templetwotaco()
+#left path---------------------------------------------------------------------
+
+def tothelefttotheleft():
+    input ("you come across a door with a lock. >")
+
+    rainbowandarrow = input ("if you know the key type it now, if not go back ").strip().lower()
+
+
+    if rainbowandarrow == ("3fh6"):
+        input ("the door unlocks and you continue into the crypt ")
+
+    elif  rainbowandarrow == ("go back"):
+        input ("you go back to find the key ")
+        
+
+
+
+
+
+
+
+
+
+
+
+templetwotaco()
