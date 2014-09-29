339a8e2b2f33f8ab02defc437f99d17e2ed2c843
mar10j <youthefluff@gmail.com>
2014-09-29 19:10:55 -0400

    I AM TALKING TO THEEEE MICHAYLA

---
+++ torch_isle.py
@@ -0,0 +1,3 @@
+def torchisshore():
+    torchislandsk = input("You arive at this large island It has a volcano, surrounded by the jungle. Would you like to head to the jungle? Explore the beach? or Sail back.[Type 'sail,'beach','jungle'").strip().lower()
+    if torchislandsk
