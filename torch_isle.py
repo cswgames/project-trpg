793352455e79c61646fd031ac8d082e81874d15f
mar10j <youthefluff@gmail.com>
2014-10-04 12:06:04 -0400

    My anaconda does.

--- torch_isle.py
+++ torch_isle.py
@@ -1,3 +1,15 @@
 def torchisshore():
     torchislandsk = input("You arive at this large island It has a volcano, surrounded by the jungle. Would you like to head to the jungle? Explore the beach? or Sail back.[Type 'sail,'beach','jungle'").strip().lower()
-    if torchislandsk
+    if torchislandsk == "sail":
+        input("Knowing the danger ahead you decide to head back.")
+        #load dock funct.
+    elif torchislandsk == "beach":
+        print("I will add this later")
+    elif torchislandsk == "jungle":
+        input("You decide to go to the jungle")
+        
+
+
+
+
+while True: torchisshore()
