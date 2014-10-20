2a2baf14d9c9c7f588efbe6bb24bdfdd9c046a78
mar10j <youthefluff@gmail.com>
2014-10-19 20:08:34 -0400

    Update
    Its been a while since I have worked on this XD

--- torch_isle.py
+++ torch_isle.py
@@ -4,12 +4,38 @@ def torchisshore():
         input("Knowing the danger ahead you decide to head back.")
         #load dock funct.
     elif torchislandsk == "beach":
-        print("I will add this later")
+        input("You decide to head to the beach")
+        contbeach()
     elif torchislandsk == "jungle":
         input("You decide to go to the jungle")
-        
+        contjung()
+    else:
+        print("I'm sorry, Let me ask you again.")
+        torchisshore()
+
+def contbeach():
+    mrbeachhassweg = input("You walk along the beach when you see strange foot prints on the sand.... do you want to follow? or keep walking foward? [Type 'follow' or 'continue']").strip().lower()
+    if mrbeachhassweg == "follow":
+        pass
+    elif mrbeachhassweg == "continue":
+        pass
+    else:
+         print("I'm sorry, Let me ask you again.")
+         contbeach()
+
+
+def contjung():
+    jungisfumkids = input("You head deep into the jungle and encounter mossy ruins. Would you like to go in? or stay on the trail? [Type 'go' or 'stay'] >").strip().lower()
+    if jungisfumkids == "go":
+        pass #load ruins funct
+    elif jungisfumkids == "stay":
+        pass
+    else:
+         print("I'm sorry, Let me ask you again.")
+         contjung()
 
 
 
 
-while True: torchisshore()
+
+torchisshore()
